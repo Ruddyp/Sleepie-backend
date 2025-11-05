950cf2d967b1db817693891c2ac45075b6182de8
Fernier <fernier.paul@gmail.com>
2025-11-05 11:36:59 +0100

    route rights get

--- routes/rights.js
+++ routes/rights.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const { checkBody } = require("../modules/checkBody");
 const User = require("../models/users");
 
+//ROUTE POUR MODIFIER LE ROLE DU USER
 router.post("/modify", async (req, res) => {
     const { token, role } = req.body;
     if (!checkBody(req.body, ["token", "role"])) {
@@ -11,9 +12,25 @@ router.post("/modify", async (req, res) => {
     try {
         const user = await User.findOne({ token: token })
         if (!user) return res.json({ result: false, message: "Aucun user trouvé" })
+        if (user.role === role) return res.json({ result: false, message: "Vous êtes déjà abonné ! " })
         user.role = role;
         await user.save()
-        res.json({ result: true, message: "rôle de l'utilisateur modifié avec succès ! " })
+        res.json({ result: true, message: "Merci pour votre abonnement ! Bon dodo " })
+    } catch (error) {
+        res.json({ result: false, error: error.message });
+    }
+})
+
+// ROUTE POUR RECUPERER LE ROLE DU USER
+router.post("/get", async (req, res) => {
+    const { token } = req.body;
+    if (!checkBody(req.body, ["token"])) {
+        return res.json({ result: false, error: "Manque le token de l'utilisateur" });
+    }
+    try {
+        const user = await User.findOne({ token: token })
+        if (!user) return res.json({ result: false, error: "Aucun user trouvé" })
+        return res.json({ result: true, role: user.role })
     } catch (error) {
         res.json({ result: false, error: error.message });
     }
